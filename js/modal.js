e276b876ca27fbffc76cff72c1c2e60933cd403a
Svetlana Novak <svetlganina@yandex.ru>
2017-10-21 16:02:52 +0300

    правка

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,18 @@
-var link = document.querySelector(".feedback-button");
+var link = document.querySelector(".feedback__button");
 var popup = document.querySelector(".modal__true");
-var close = document.querySelector(".modal-close");
+var popupFalse = document.querySelector(".modal__false");
+var close = document.querySelector(".modal__close");
 
 link.addEventListener("click", function(evt){
   evt.preventDefault();
-  popup.classList.add("modal-show");
+  popup.classList.add("modal__show");
 });
 close.addEventListener("click", function(evt){
   evt.preventDefault();
-  popup.classList.remove("modal-show");
+  popup.classList.remove("modal__show");
 });
 window.addEventListener("keydown", function(evt){
-  if (evt.keyCode === 27 && popup.classList.contains("modal-show")){
-    popup.classList.remove("modal-show");
+  if (evt.keyCode === 27 && popup.classList.contains("modal__show")){
+    popup.classList.remove("modal__show");
   }
 });
