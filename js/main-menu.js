445068f841daf381b6a94769c22497b75b0d2d9f
Svetlana Novak <svetlganina@yandex.ru>
2017-10-26 23:19:12 +0300

    меню

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,4 +1,5 @@
-var link = document.querySelector(".main-nav__toggle");
+var buttonMenuClosed = document.querySelector(".main-nav__toggle--closed");
+var buttonMenuOpen = document.querySelector(".main-nav__toggle--opend");
 var popup = document.querySelector(".main-nav");
 var listLeft = document.querySelector(".site-list--left");
 var listRight = document.querySelector(".site-list--right");
@@ -6,16 +7,15 @@ var listRight = document.querySelector(".site-list--right");
 link.addEventListener("click", function(evt){
 evt.preventDefault();
 
-if (popup.classList.contains("main-nav--opend")){
 popup.classList.remove("main-menu--opend");
-popup.classList.add("main-menu--closed");
 listLeft.classList.add("main-nav__list--closed");
 listRight.classList.add("main-nav__list--closed");
-}
-else {
-popup.classList.remove("main-menu--closed");
-popup.classList.add("main-menu--opend");
-listLeft.classList.remove("main-nav__list--closed");
-listRight.classList.remove("main-nav__list--closed");
-}
+});
+
+link.addEventListener("click", function(evt){
+evt.preventDefault();
+
+popup.classList.remove("main-menu--opend");
+listLeft.classList.add("main-nav__list--closed");
+listRight.classList.add("main-nav__list--closed");
 });
